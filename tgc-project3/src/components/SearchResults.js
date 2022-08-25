55dbadde12d35280c4dbe245b481e770d5583982
Koihcire <ericchiok@gmail.com>
2022-08-25 09:03:36 +0000

    show products in card

--- tgc-project3/src/components/SearchResults.js
+++ tgc-project3/src/components/SearchResults.js
@@ -5,13 +5,26 @@ import { Fragment } from 'react'
 export default function SearchResults(props) {
     return (
         <Fragment>
-            <ul>
+            {/* <ul> */}
+            <div className='row justify-content-around'>
                 {props.products.map(m => {
-                    return <li>
-                        <Link to={'/product/' + m.id}>{m.product}</Link>
-                    </li>
+                    return (
+                        // <li>
+                        //     <Link to={'/product/' + m.id}>{m.product}</Link>
+                        // </li>
+                        <div className="card col-12 col-md-5 col-lg-4" style={{border:"none"}}>
+                            <img src={m.product_image_url} alt="productPhoto"></img>
+                            <div className="card-body">
+                                <h5 className="card-title">{m.product}</h5>
+                                <p className="card-text">{m.description}</p>
+                                <a href={`/product/` + m.id} className="btn btn-primary btm-sm">View</a>
+                            </div>
+                        </div>
+                    )
                 })}
-            </ul>
+            </div>
+
+            {/* </ul> */}
         </Fragment>
     )
 }
