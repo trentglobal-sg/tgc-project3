813122426083584949e32a6c5cc3c904ce925fd1
Koihcire <ericchiok@gmail.com>
2022-08-31 03:22:53 +0000

    test

--- tgc-project3/src/pages/Orders.js
+++ tgc-project3/src/pages/Orders.js
@@ -8,23 +8,23 @@ export default function Orders() {
     const navigate = useNavigate()
     const customerContext = useContext(CustomerContext)
     const [orders, setOrders] = useState('')
-    const [activeOrder, setActiveOrder] = useState('')
+    // const [activeOrder, setActiveOrder] = useState('')
 
-    const [show, setShow] = useState(false);
-    const handleClose = () => {
-        setActiveOrder('')
-        setShow(false);
-    }
-    const handleShow = (id) => {
-        let selectedOrder = [];
-        for (let order of orders) {
-            if (order.id == id) {
-                selectedOrder.push(order)
-            }
-        }
-        setActiveOrder(selectedOrder)
-        setShow(true);
-    }
+    // const [show, setShow] = useState(false);
+    // const handleClose = () => {
+    //     setActiveOrder('')
+    //     setShow(false);
+    // }
+    // const handleShow = (id) => {
+    //     let selectedOrder = [];
+    //     for (let order of orders) {
+    //         if (order.id == id) {
+    //             selectedOrder.push(order)
+    //         }
+    //     }
+    //     setActiveOrder(selectedOrder)
+    //     setShow(true);
+    // }
 
     const getOrdersData = async () => {
         let response = await customerContext.getOrders()
@@ -54,10 +54,8 @@ export default function Orders() {
                             <th>ID</th>
                             <th>Status</th>
                             <th>Order Date</th>
-                            <th>Updated Date</th>
                             <th>Amount</th>
                             <th>Shipping Type</th>
-                            <th></th>
                             <th></th>
                         </tr>
                     </thead>
@@ -65,31 +63,29 @@ export default function Orders() {
                         {orders ? orders.map(order => {
                             return <tr>
                                 <td>{order.id}</td>
-                                <td>{order.order_status.order_status}</td>
+                                <td>{order.order_status.order_status} {order.updated_date ? <td>({moment(order.updated_date).format('YYYY-MM-DD')})</td> : ''}</td>
                                 <td>{moment(order.order_date).format('YYYY-MM-DD')}</td>
-                                {order.updated_date ? <td>{moment(order.updated_date).format('YYYY-MM-DD')}</td> : <td><p>As Order Date</p></td>}
                                 <td>$ {order.total_amount / 100}</td>
-                                <td>{order.shipping_type}</td>
-                                <td><button className='btn btn-sm btn-primary' onClick={()=>openInNewTab(order.receipt_url)}>Receipt</button></td>
-                                <td><button className="btn btn-sm btn-primary" onClick={() => { handleShow(order.id) }}>More</button></td>
+                                <td>{order.shipping_type} <br></br> {order.shipping_address_line1} <br></br>{order.shipping_address_line2} <br></br>{order.shipping_postal_code} <br></br>{order.shipping_country}</td>
+                                <td><button className='btn btn-sm btn-primary' onClick={() => openInNewTab(order.receipt_url)}>Receipt</button></td>
                             </tr>
                         })
                             : ''}
                     </tbody>
                 </table>
             </div>
-            {activeOrder.length ?
-                <Modal show={show} onHide={handleClose} style={{ marginTop: '60px' }}>
-                    <Modal.Header closeButton>
+            {/* {activeOrder.length ? */}
+                {/* <Modal show={show} onHide={handleClose} style={{ marginTop: '60px' }}> */}
+                    {/* <Modal.Header closeButton> */}
                         {/* <Modal.Title>{activeOrder.order_id}</Modal.Title> */}
-                    </Modal.Header>
-                    <Modal.Body>{activeOrder[0].order_status.order_status}</Modal.Body>
-                    <Modal.Footer>
-                        <button variant="secondary" onClick={handleClose}>
-                            Close
-                        </button>
-                    </Modal.Footer>
-                </Modal> : ''}
+                    {/* </Modal.Header> */}
+                    {/* <Modal.Body>{activeOrder[0].order_status.order_status}</Modal.Body> */}
+                    {/* <Modal.Footer> */}
+                        {/* <button variant="secondary" onClick={handleClose}> */}
+                            {/* Close */}
+                        {/* </button> */}
+                    {/* </Modal.Footer> */}
+                {/* </Modal> : ''} */}
 
         </Fragment>
     )
