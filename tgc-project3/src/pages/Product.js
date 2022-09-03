107ae0ccaa691796df4649d2ffec516ff34c403e
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-09-03 12:00:55 +0800

    centralised number input for quantity on products page

--- tgc-project3/src/pages/Product.js
+++ tgc-project3/src/pages/Product.js
@@ -204,7 +204,7 @@ export default function Product(props) {
                                             <button className='btn btn-sm my-btn' onClick={decrement} ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z" />
                                             </svg></button>
-                                            <input type="number" style={{ width: '50px' }} className='form-input form-input-sm ms-2 me-2' value={selectedQuantity} onChange={(e) => { setSelectedQuantity(e.target.value) }} disabled />
+                                            <input type="number" style={{ width: '40px', textAlign:'right' }} className='form-input form-input-sm ms-2 me-2' value={selectedQuantity} onChange={(e) => { setSelectedQuantity(e.target.value) }} disabled />
                                             <button className='btn btn-sm my-btn' onClick={increment}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
                                             </svg></button>
